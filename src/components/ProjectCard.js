ad9c18f97f71e92014ecad26db2f4dd3274259bb
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-11-30 16:27:10 -0500

    Built basic ProjectCard component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,10 @@
+function ProjectCard({ title, description, link, image }) {
+    return (
+        <div>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href={link} target="_blank" rel="noopener noreferrer">View Project</a>
+        </div>
+    )
+}
