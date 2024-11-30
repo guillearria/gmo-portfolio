fc65d5de23d9dc7235b3d6d54088b939d7aa82af
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-11-30 14:40:24 -0500

    Create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <h2>G'Mo AI Portfolio</h2>
+            <ul>
+                <li><a href="#about" style={{}}>About</a></li>
+                <li><a href="#about" style={{}}>Projects</a></li>
+                <li><a href="https://www.linkedin.com/in/guillearria/" style={{}}>LinkedIn</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
