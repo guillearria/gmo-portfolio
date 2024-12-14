3acc16239e98c84340425157d7ff1e518fa9e7ff
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-12-14 13:29:01 -0500

    Created fetchRandomPoem

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,6 @@
-export const fetchOzymandias = async () => {
-    try {
-        const response = await fetch("https://poetrydb.org/title/Ozymandias/lines.json");
-        if (!response.ok) throw new Error(`Failed to fetch results: ${response.status}`);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(`Error fetching results: ${error}`);
-        return null;
-    }
-};
+// POETRYDB SERVICES
 
-// Fetch list of PoetryDB authors
+// Fetch list of poem authors
 export const fetchAuthors = async () => {
     try {
         const response = await fetch("https://poetrydb.org/author");
@@ -23,10 +13,36 @@ export const fetchAuthors = async () => {
     }
 };
 
-// Fetch list of PoetryDB authors
-export const fetchTitles = async (author) => {
+// Fetch list of poem titles
+export const fetchTitles = async (authorId) => {
     try {
-        const response = await fetch(`https://poetrydb.org/${author}/title`);
+        const response = await fetch(`https://poetrydb.org/${authorId}/title`);
+        if (!response.ok) throw new Error(`Failed to fetch results: ${response.status}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching results: ${error}`);
+        return null;
+    }
+};
+
+// Fetch list of lines from a poem
+export const fetchLines = async (titleId) => {
+    try {
+        const response = await fetch(`https://poetrydb.org/title/${titleId}/lines.json`);
+        if (!response.ok) throw new Error(`Failed to fetch results: ${response.status}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching results: ${error}`);
+        return null;
+    }
+};
+
+// Fetch one random poem
+export const fetchRandomPoem = async () => {
+    try {
+        const response = await fetch(`https://poetrydb.org/random/1`);
         if (!response.ok) throw new Error(`Failed to fetch results: ${response.status}`);
         const data = await response.json();
         return data;
