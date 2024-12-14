7ea5ce4080ead3850166faff51500471e0d5af4c
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-12-14 13:14:39 -0500

    Create fetchAuthors function

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,24 @@
 export const fetchOzymandias = async () => {
     try {
         const response = await fetch("https://poetrydb.org/title/Ozymandias/lines.json");
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
+        if (!response.ok) throw new Error(`Failed to fetch results: ${response.status}`);
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error(`Fetch error: ${error}`);
-        return {}
+        console.error(`Error fetching results: ${error}`);
+        return null
+    }
+};
+
+// Fetch list of PoetryDB authors
+export const fetchAuthors = async () => {
+    try {
+        const response = await fetch("https://poetrydb.org/author");
+        if (!response.ok) throw new Error(`Failed to fetch results: ${response.status}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error fetching results: ${error}`);
+        return null
     }
 }
